ac0cc2989a76b9bfe01527a1c98c3d222138cfb7
BrannanC <brannanconrad@gmail.com>
2019-02-22 11:13:58 -0600

    API set up can get project with actions, Post project and actions

--- index.js
+++ index.js
@@ -12,15 +12,63 @@ server.use(helmet());
 
 // POST for adding projects.
 
-
+server.post('/api/projects', (req, res) => {
+    const project = req.body;
+    if(!project.projectName || !project.description){
+        res.status(400).json({ error: 'Please provide project name and description' })
+    } else {
+        db('projects') 
+        .insert(project)
+        .then(id => {
+            res.status(201).json({ id: id[0] })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'Could not add project' })
+        })
+    }
+});
 
 // POST for adding actions.
 
+server.post('/api/projects/:id', (req, res) => {
+    const action = {...req.body, projectId: req.params.id};
+    if(!action.action){
+        res.status(400).json({ error: 'Please provide action name' })
+    } else {
+        db('actions') 
+        .insert(action)
+        .then(id => {
+            res.status(201).json({ id: id[0] })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'Could not add action', action })
+        })
+    }
+});
+
 // GET for retrieving a project
+server.get('/api/projects/:id', (req, res) => {
+
+    db('projects').where('id', req.params.id).first()
+        .then(project => {
+            db('actions')
+                .where('projectId', req.params.id)
+                .then(actions => {
+                    res.status(200).json({ project: {...project, actions} })
+                })            
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: 'Could not get projects' })
+        })
+});
+
 server.get('/api/projects', (req, res) => {
     db('projects')
         .then(projects => {
-            res.status(200).json({ projects })
+            res.status(200).json({ projects })         
         })
         .catch(err => {
             console.log(err);
--- migrations/20190222101114_actions_table.js
+++ migrations/20190222101114_actions_table.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
         tbl.integer('projectId').unsigned().references('id').inTable('projects')
           .onDelete('CASCADE').onUpdate('CASCADE').notNullable();
         tbl.string('action', 50).unique().notNullable();
-        tbl.string('notes', 255).unique().notNullable();
+        tbl.string('notes', 255).unique();
         tbl.boolean('isComplete').notNullable().defaultTo(false);
         tbl.timestamp('createdAt').defaultTo(knex.fn.now());
       })
